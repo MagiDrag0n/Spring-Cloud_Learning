fbaa37376955a90a503a8fb51158a4579c7ace30
magidragon <magidragon@foxmail.com>
2022-04-11 16:01:28 +0800

    [UPDATE]Eureka整合子工程

--- cloud-consumer-order-8002/src/main/java/com/orderMain8002.java
+++ cloud-consumer-order-8002/src/main/java/com/orderMain8002.java
@@ -2,8 +2,10 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class orderMain8002 {
     public static void main(String[] args) {
         SpringApplication.run(orderMain8002.class,args);
--- cloud-provider-payment-8001/src/main/java/com/paymentMain8001.java
+++ cloud-provider-payment-8001/src/main/java/com/paymentMain8001.java
@@ -2,9 +2,11 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class paymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(paymentMain8001.class, args);
