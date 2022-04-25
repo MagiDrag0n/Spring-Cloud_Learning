6ea3ba3af99ef7cd89cedc2b28dec3504fc51ca8
magidragon <magidragon@foxmail.com>
2022-04-25 22:13:09 +0800

    [UPDATE]order 服务熔断

--- cloud-consumer-feign-hystrix-order-8101/src/main/java/com/service/fallback/PaymentFallbackHystrixService.java
+++ cloud-consumer-feign-hystrix-order-8101/src/main/java/com/service/fallback/PaymentFallbackHystrixService.java
@@ -1,8 +1,12 @@
 package com.service.fallback;
 
+import cn.hutool.core.util.IdUtil;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.service.paymentHystrixService;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
 public class PaymentFallbackHystrixService implements paymentHystrixService {
@@ -15,4 +19,22 @@ public class PaymentFallbackHystrixService implements paymentHystrixService {
     public String paymentInfo_ERROR(Integer id) {
         return "-------PaymentFallbackService fall back-paymentInfo_TimeOut,o(╥﹏╥)o";
     }
+
+    //服务熔断
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enable",value = "true"),//是否启用断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),//请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),//时间窗口期
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60")//失败率达到多少后跳闸
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        if(id<0)
+            throw new RuntimeException("****id 不能为负数");
+        String serialNumber = IdUtil.simpleUUID();//hutool工具
+        return Thread.currentThread().getName()+"\t"+"调用成功，流水号："+serialNumber;
+    }
+    public String paymentCircuitBreaker_fallback(Integer id){
+        return "id："+id+"，不能为负数，请稍后再试，。。。";
+    }
+
 }
